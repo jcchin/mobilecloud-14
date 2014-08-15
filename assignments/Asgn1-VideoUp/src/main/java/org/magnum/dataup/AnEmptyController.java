33c4f31d785d3c8e3c9123ba6aa5ed8c0bdc5f29
Jeff Chin <jchin89@gmail.com>
2014-08-15 18:11:05 -0400

    working 2+ 4

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/AnEmptyController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/AnEmptyController.java
@@ -17,26 +17,108 @@
  */
 package org.magnum.dataup;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @Controller
 public class AnEmptyController {
-
-	/**
-	 * You will need to create one or more Spring controllers to fulfill the
-	 * requirements of the assignment. If you use this file, please rename it
-	 * to something other than "AnEmptyController"
-	 * 
-	 * 
-		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
-		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
-		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
-		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
-		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
-		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
-		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
-                                                                                                                                                                                                                                                                        
-	 * 
-	 */
+	private VideoFileManager videoDataMgr;
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	private Map<Long,Video> videos = new HashMap<Long, Video>();
 	
+	@RequestMapping(value="/video", method=RequestMethod.GET)
+	public @ResponseBody Collection getVideoList(){
+		return videos.values();
+	}
+	
+	@RequestMapping(value="/video", method=RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+		save(v);
+		v.setDataUrl(getDataUrl(v.getId()));
+		return v;
+	}
+	
+	@RequestMapping(value="/video/{id}/data", method=RequestMethod.POST)
+	public @ResponseBody VideoStatus setVideoData(@RequestBody Video v, @PathVariable("id") long id, @RequestParam("data") MultipartFile videoData){
+		try {
+            videoDataMgr = VideoFileManager.get();
+            saveSomeVideo(v,videoData);
+            return new VideoStatus(VideoState.READY);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }	
+	}
+	
+	@RequestMapping(value="/video/{id}/data", method=RequestMethod.GET)
+	public @ResponseBody void getData(@RequestBody Video v, @PathVariable("id") long id, HttpServletResponse response) throws IOException {
+		try {
+            videoDataMgr = VideoFileManager.get();
+            serveSomeVideo(v,response);
+		 } catch (IOException e) {
+			 e.printStackTrace();
+         }
+	}
+	
+	// Initialize this member variable somewhere with 
+    // videoDataMgr = VideoFileManager.get()
+    //
+
+    // You would need some Controller method to call this...
+  	public void saveSomeVideo(Video v, MultipartFile videoData) throws IOException {
+  	     videoDataMgr.saveVideoData(v, videoData.getInputStream());
+  	}
+  	
+  	public void serveSomeVideo(Video v, HttpServletResponse response) throws IOException {
+  	     // Of course, you would need to send some headers, etc. to the
+  	     // client too!
+  	     //  ...
+  	     videoDataMgr.copyVideoData(v, response.getOutputStream());
+  	}
+	
+	public Video save(Video entity) {
+        checkAndSetId(entity);
+        videos.put(entity.getId(), entity);
+        return entity;
+	}
+	private void checkAndSetId(Video entity) {
+		if(entity.getId() == 0){
+			entity.setId(currentId.incrementAndGet());
+        }
+	 }
+	private String getDataUrl(long videoId){
+        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+        return url;
+    }
+
+ 	private String getUrlBaseForLocalServer() {
+	   HttpServletRequest request = 
+	       ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+	   String base = 
+	      "http://"+request.getServerName() 
+	      + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+	   return base;
+	}
+
 }
